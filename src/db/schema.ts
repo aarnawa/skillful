a65ee7e00ee4b0f7c7a5f9cfeff9bf2570be331d
aarnawa05 <aarnawakoirala@gmail.com>
2026-03-31 22:22:12 -0500

    Added password field to schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,6 +5,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 16 }).notNull().default("CHANGEME"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
